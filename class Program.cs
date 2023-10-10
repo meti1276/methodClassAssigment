76d0a1e6b914bfe6fbab05a887e59e536f904ab2
meti1276 <134025235+meti1276@users.noreply.github.com>
2023-10-10 13:32:44 -0400

    Add files via upload

---
+++ class Program.cs
@@ -0,0 +1,23 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathMethod mathMethod = new MathMethod(); //Instantiate
+
+    }
+}
+
+
+class MathMethod
+{
+    public void Operator()
+    {
+        int num1 = 6;
+        int num2 = 9;
+    }
+    public void Output(int number1, int number2)
+    {
+        int value = 
+    }
+
+
